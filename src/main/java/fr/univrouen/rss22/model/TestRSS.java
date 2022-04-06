bc58c2605666cb1f56e2a0c373d0d5980f583280
Younext19 <youneshaddam9@gmail.com>
2022-04-06 11:34:22 +0200

    load file xml changes

--- src/main/java/fr/univrouen/rss22/model/TestRSS.java
+++ src/main/java/fr/univrouen/rss22/model/TestRSS.java
@@ -2,13 +2,27 @@ package fr.univrouen.rss22.model;
 
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class TestRSS {
 
-    public Resource loadFileXml(Resource resource){
-        resource = new DefaultResourceLoader().getResource("classpath:xml/item.xml");
-        return resource;
-    }
 
+    public String loadFileXML() {
+        org.springframework.core.io.Resource resource;
+        resource = new DefaultResourceLoader().getResource("classpath:xml/item.xml");
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+
+    }
 
 }
